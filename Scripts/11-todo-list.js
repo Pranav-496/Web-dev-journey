1380a85a8e40b5e0133f656f45ae8688491e3b8d
Pranav-496 <pranavlandge79@gmail.com>
2025-08-19 23:46:41 +0530

    Done with todo list

--- Scripts/11-todo-list.js
+++ Scripts/11-todo-list.js
@@ -1,15 +1,24 @@
-const todoList = ['make', 'dinner'];
+const todoList = [];
 renderTodoList();
 
 function renderTodoList(params) {
-
-
-
+    
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`; // this technique is called as generating the html using javascript
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject; // same as above , saves lines
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>            
+           <button onclick="
+             todoList.splice(${i},1);
+             renderTodoList();
+           " class="delete-button">Delete</button>
+        
+        `; // this technique is called as generating the html using javascript
         todoListHTML += html;
     }
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
@@ -17,9 +26,15 @@ function renderTodoList(params) {
 
 function addTodo() {
     const inputEle = document.querySelector('.js-name-input');
+    const dateInputEle = document.querySelector('.js-due-date-input');
     const name = inputEle.value;
-    todoList.push(name);
-    console.log(todoList);
+    const dueDate = dateInputEle.value;
+    todoList.push({//name : name,
+        //dueDate : dueDate
+        name,
+        dueDate // shorthand syntax
+    });
+    //console.log(todoList);
 
     inputEle.value = ''; //empties the input element
     renderTodoList();
